8d03a19864eaafee04d6186c6461a07bbd8d61d6
PedroAlmeida08 <74750373+PedroAlmeida08@users.noreply.github.com>
2025-09-10 10:35:06 -0300

    Add files via upload

---
+++ mpi_barrier.c
@@ -0,0 +1,26 @@
+/*
+  Para compilar e executar:
+    mpicc -o mpi_barrier mpi_barrier.c
+    mpirun -n 8 mpi_barrier
+*/ 
+
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+int meu_ranque, num_procs;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&meu_ranque);
+    
+    if (meu_ranque == 0 ) {
+        printf ("Estou atrasado para a barreira! \n");
+        getchar();
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Passei da barreira. Eu sou o %d de %d processos \n", meu_ranque, num_procs);
+  
+    MPI_Finalize();
+    return(0);
+}
